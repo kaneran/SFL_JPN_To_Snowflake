94ce95de8019eb0bbaea59c9ed23618c2766dcca
Kane <95705546+kaneran@users.noreply.github.com>
2022-08-07 10:48:02 +0100

    Created script to extract data from csv into snowflake data warehouse

---
+++ index.js
@@ -0,0 +1,53 @@
+const snowflake = require("snowflake-sdk");
+const fs = require("fs");
+require("dotenv").config();
+
+const { ACCOUNT, USER, PASSWORD, DATABASE, SCHEMA } = process.env;
+
+const connection = snowflake.createConnection({
+  account: ACCOUNT,
+  username: USER,
+  password: PASSWORD,
+  database: DATABASE,
+  schema: SCHEMA,
+});
+
+connection.connect((err, conn) => {
+  if (err) {
+    console.error("Unable to connect: " + err.message);
+  } else {
+    console.log("Successfully connected to Snowflake.");
+  }
+});
+
+const readCsv = () => {
+  const data = fs.readFileSync("sfv_jpn_matches.csv", "utf8");
+  let dataArr = [];
+  const results = data.split("\n");
+  results.forEach((result) => {
+    const data = result.split(",");
+    const result_id = parseInt(data[0]);
+    const matches_won = parseInt(data[3]);
+    const outcome = data[4]?.replace("\r", "");
+    const arr = [result_id, data[1], data[2], matches_won, outcome];
+    data[0] !== "" && data[1] !== "Player" && dataArr.push(arr);
+  });
+  return dataArr;
+};
+
+const data = readCsv();
+connection.execute({
+  sqlText:
+    "insert into MATCHRESULTS(RESULT_ID, PLAYER, CHARACTER, MATCHES_WON, OUTCOME) values(?, ?, ?, ?, ?)",
+  binds: data,
+  complete: (err, stmt, rows) => {
+    if (err) {
+      console.error(
+        "Failed to execute statement due to the following error: " + err.message
+      );
+    } else {
+      console.log("Number of rows produced: " + rows.length);
+    }
+  },
+});
+connection.destroy();
